bd788e69b4fbdd82690fcd23e6ace895a1087857
Miguel Prada <miguel.prada.sarasola@gmail.com>
2016-05-12 17:45:16 +0200

    Get handle to receptacle joint from slider model plugin, in order to be able to read the forces from there without dynamic components.
    REQUIRES THE RECEPTACLE MODEL TO EXIST BEFORE THE SLIDER IS SPAWNED!!

--- planar_folding_assembly/src/planar_folding_assembly_controller_plugin.cpp
+++ planar_folding_assembly/src/planar_folding_assembly_controller_plugin.cpp
@@ -14,29 +14,32 @@ class PlanarFoldingAssemblyControllerPlugin : public ModelPlugin
 
 public:
 
-  void Load( physics::ModelPtr parent, sdf::ElementPtr sdf )
+  void Load( physics::ModelPtr model, sdf::ElementPtr sdf )
   {
 
-    model_ = parent;
+    model_ = model;
+
+    slider_handle_link_ = model_->GetLink( "slider_handle" );
+    slider_link_ = model_->GetLink( "slider" );
+
+    slider_joint_ = model_->GetJoint( "slider_joint" );
+
+    receptacle_joint_ = model_->GetWorld()->GetModel( "receptacle" )->GetJoint( "receptacle_joint" );
+
+    slider_joint_->SetProvideFeedback( true );
+    receptacle_joint_->SetProvideFeedback( true );
 
     updateConnection_ = event::Events::ConnectWorldUpdateBegin( boost::bind( &PlanarFoldingAssemblyControllerPlugin::onUpdate, this, _1 ) );
-
-    handle_link_ = model_->GetLink( "slider_handle" );
-    box_link_ = model_->GetLink( "slider" );
-
-    handle_joint_ = model_->GetJoint( "slider_joint" );
-
-    handle_joint_->SetProvideFeedback( true );
 
   }
 
   void onUpdate( const common::UpdateInfo info )
   {
 
-    handle_link_->SetLinearVel( math::Vector3( 0, 0, -0.1 ) );
+    slider_handle_link_->SetLinearVel( math::Vector3( 0, 0, -0.1 ) );
 
-    math::Vector3 fb1 = handle_joint_->GetForceTorque(0).body1Force;
-    math::Vector3 fb2 = handle_joint_->GetForceTorque(0).body2Force;
+    math::Vector3 fb1 = receptacle_joint_->GetForceTorque(0).body1Force;
+    math::Vector3 fb2 = receptacle_joint_->GetForceTorque(0).body2Force;
 
     std::cout << "fb1: " << fb1.x << ", " << fb1.y << ", " << fb1.z << std::endl;
     std::cout << "fb2: " << fb2.x << ", " << fb2.y << ", " << fb2.z << std::endl;
@@ -47,10 +50,12 @@ private:
 
   physics::ModelPtr model_;
 
-  physics::LinkPtr handle_link_;
-  physics::LinkPtr box_link_;
+  physics::LinkPtr slider_handle_link_;
+  physics::LinkPtr slider_link_;
 
-  physics::JointPtr handle_joint_;
+  physics::JointPtr slider_joint_;
+
+  physics::JointPtr receptacle_joint_;
 
   event::ConnectionPtr updateConnection_;
 
