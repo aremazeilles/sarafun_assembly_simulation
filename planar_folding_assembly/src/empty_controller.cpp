8a9677f73747e489aabb9e293e68d24dbfe40292
Miguel Prada <miguel.prada.sarasola@gmail.com>
2016-05-13 15:34:29 +0200

    Restructure into a base plugin and derived controllers. ToDo: document how to add more controllers.

---
+++ planar_folding_assembly/src/empty_controller.cpp
@@ -0,0 +1,13 @@
+#include "planar_folding_assembly_base_plugin.h"
+
+namespace gazebo
+{
+  
+class EmptyController : public PlanarFoldingAssemblyBasePlugin
+{
+  
+};
+
+GZ_REGISTER_MODEL_PLUGIN( EmptyController )
+
+}
